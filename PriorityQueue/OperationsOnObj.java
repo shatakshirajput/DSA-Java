855fb478363e1cceda15ec668851dff6cbcb8dd1
Shatakshi Rajput <shatakshirajput02@gmail.com>
2025-08-20 11:03:54 +0530

    PQObj

--- PriorityQueue/OperationsOnObj.java
+++ PriorityQueue/OperationsOnObj.java
@@ -1,7 +1,34 @@
 package PriorityQueue;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class OperationsOnObj {
+    static class Student implements Comparable<Student>{
+        String name;
+        int rank;
+
+        public Student(String name, int rank){
+            this.name= name;
+            this.rank = rank;
+        }
+
+        @Override
+        public int compareTo(Student s2){
+            return this.rank - s2.rank;
+        }
+    }
     public static void main(String[] args) {
-        
+        PriorityQueue<Student> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        pq.add(new Student("A",4));
+        pq.add(new Student("B",3));
+        pq.add(new Student("C",6));
+        pq.add(new Student("D",2));
+
+        while(!pq.isEmpty()){
+            System.out.println(pq.peek().name + " -> "+ pq.peek().rank);
+            pq.remove();
+        }
     }
 }
