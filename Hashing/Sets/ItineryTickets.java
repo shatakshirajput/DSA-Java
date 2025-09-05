fdbad7f4e49b34cb174530e53c1158df81e5e714
Shatakshi Rajput <shatakshirajput02@gmail.com>
2025-09-05 10:52:42 +0530

    set

--- Hashing/Sets/ItineryTickets.java
+++ Hashing/Sets/ItineryTickets.java
@@ -4,31 +4,37 @@ import java.util.HashMap;
 
 public class ItineryTickets {
 
-    public static String getStart(HashMap<String,String> tickets){
-        HashMap<String ,String> rev = new HashMap<>();
-        for(String key : rev.keySet()){
-            rev.put(tickets.get(key),key);
+    public static String getStart(HashMap<String, String> tickets) {
+        HashMap<String, String> rev = new HashMap<>();
+        for (String key : tickets.keySet()) {
+            rev.put(tickets.get(key), key);
         }
-        for(String k: tickets.keySet()){
-            if(!rev.containsKey(k)){
+        for (String k : tickets.keySet()) {
+            if (!rev.containsKey(k)) {
                 return k;
             }
         }
         return null;
     }
-    public static void main(String[] args){
-        HashMap<String,String> tickets= new HashMap<>();
-        tickets.put("C","B");
-        tickets.put("M","D");
-        tickets.put("G","C");
-        tickets.put("D","G");
 
-        String s = getStart(tickets);
-        System.out.println(s);
-        for(String k : tickets.keySet()){
-            System.out.print(" -> "+tickets.get(s));
+    public static void main(String[] args) {
+        HashMap<String, String> tickets = new HashMap<>();
+        tickets.put("C", "B");
+        tickets.put("M", "D");
+        tickets.put("G", "C");
+        tickets.put("D", "G");
+
+        String start = getStart(tickets);
+        System.out.println("Starting city: " + start);
+
+        String s = start;
+        while (s != null) {
+            System.out.print(s);
             s = tickets.get(s);
-        }      
+            if (s != null) {
+                System.out.print(" -> ");
+            }
+        }
         System.out.println();
     }
 }
