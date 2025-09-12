6210fa1ca9066956118e158955c3fb5a5178b3cb
Shatakshi Rajput <shatakshirajput02@gmail.com>
2025-09-12 21:10:42 +0530

    buildingtree

--- BinaryTrees/BuildTree.java
+++ BinaryTrees/BuildTree.java
@@ -26,6 +26,7 @@ public class BuildTree {
             return newNode;
         }
     }
+
     public static void main(String[] args) {
         // int[] nodes ={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         // BT tree = new BT();
