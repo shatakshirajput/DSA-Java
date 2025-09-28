3360d08aad9fcc8354befc51cd66d080419d249e
Shatakshi Rajput <shatakshirajput02@gmail.com>
2025-09-28 19:24:00 +0530

    Trie basics

--- Tries/Basic.java
+++ Tries/Basic.java
@@ -1,5 +1,20 @@
 package Tries;
 
 public class Basic {
-    
+    static class Node{
+        Node children[] = new Node[26];
+        boolean eow = false;
+
+        Node(){
+            for(int i =0;i<26;i++){
+                children[i] =  null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void main(String[] args){
+
+    }
 }
