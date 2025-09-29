606eb41476f34b00b1bf82cade3b07628a291d3e
Shatakshi Rajput <shatakshirajput02@gmail.com>
2025-09-29 12:41:03 +0530

    trie search update

--- Tries/Basic.java
+++ Tries/Basic.java
@@ -12,9 +12,36 @@ public class Basic {
         }
     }
 
-    public static Node root = new Node();
+    static Node root = new Node();
+    public static void insert(String word){
+        Node curr = root;
+        for(int level=0;level<word.length();level++){
+            int idx = word.charAt(level) - 'a';
+            if(curr.children[idx] == null){
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
 
+    public static boolean search(String key){
+        Node curr = root;
+        for(int level =0;level<key.length();level++){
+            int idx = key.charAt(level) - 'a';
+            if(curr.children[idx] == null){
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.eow == true;
+    }
     public static void main(String[] args){
-        int[] words = {10};
+        String[] words = {"thr","a","there","their","any","thee"};
+        for (String word : words) {
+            insert(word);
+        }
+        System.out.println(search("thee"));
+        System.out.println(search("thor"));
     }
 }
