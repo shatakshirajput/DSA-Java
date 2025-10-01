a1cf03c3d0d3b8abba75a6ebf726a54d689c8d44
Shatakshi Rajput <shatakshirajput02@gmail.com>
2025-10-01 16:13:14 +0530

    trie

--- Tries/PrefixProb.java
+++ Tries/PrefixProb.java
@@ -3,7 +3,7 @@ package Tries;
 public class PrefixProb {
     static class Node{
         Node[] children = new Node[26];
-        boolean eow = false;
+        // boolean eow = false;
         int freq ;
 
         public Node(){
@@ -14,11 +14,11 @@ public class PrefixProb {
         }
     }
 
-    public static Node root = new Node();
+    static Node root = new Node();
 
     public static void insert(String word){
         Node curr = root;
-        for(int i =0;i<word.charAt(i);i++){
+        for(int i =0;i<word.length();i++){
             int idx = word.charAt(i) - 'a';
             if(curr.children[idx]==null){
                 curr.children[idx]=new Node();
@@ -28,10 +28,10 @@ public class PrefixProb {
             }
             curr = curr.children[idx];
         }
-        curr.eow = true;
+        // curr.eow = true;
     }
 
-    public static void findPrefix(Node root,String ans){
+    static void findPrefix(Node root,String ans){
         if(root == null){
             return;
         }
@@ -39,14 +39,19 @@ public class PrefixProb {
             System.out.println(ans);
             return ;
         }
-        for(int i=0;i<26;i++){
+        for(int i=0;i < root.children.length;i++){
             if(root.children[i]!= null){
-                
+                findPrefix(root.children[i],ans+(char)(i+'a'));
             }
         }
     }
 
     public static void main(String[] args){
-
+        String arr[] = {"zebra","dog","duck","dove"};
+        for (String arr1 : arr) {
+            insert(arr1);
+        }
+        root.freq = -1;
+        findPrefix(root,"");
     }
 }
---
+++ Tries/StartsWith.java
@@ -0,0 +1,65 @@
+package Tries;
+
+public class StartsWith {
+
+    static class Node{
+        Node[] children = new Node[26];
+        // boolean eow;
+
+        Node(){
+            for(int i = 0;i<children.length;i++){
+                children[i]=null;
+            }
+        }
+    }
+
+    static Node root = new Node();
+
+    public static void insert(String word){
+        Node curr = root;
+        for(int i =0;i<word.length();i++){
+            int idx = word.charAt(i)-'a';
+            if(curr.children[idx] == null){
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        // curr.eow = true;
+    }
+
+    public static boolean search(String key){
+        Node curr = root;
+        for(int i =0;i < key.length();i++){
+            int idx = key.charAt(i)-'a';
+            if(curr.children[idx] == null){
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return true;
+    }
+
+    public static boolean startsWith(String prefix){
+        Node curr = root;
+        for(int i =0;i < prefix.length();i++){
+            int idx = prefix.charAt(i)-'a';
+            if(curr.children[idx] == null){
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        String words[] = {"apple","app","man","mango","woman"};
+        String prefix1 ="app";
+        String prefix2 = "moon";
+
+        for (String word : words) {
+            insert(word);
+        }
+
+        startsWith(prefix1);
+        startsWith(prefix2);
+    }
+}
