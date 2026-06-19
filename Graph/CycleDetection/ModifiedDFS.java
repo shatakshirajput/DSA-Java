a53e230b92151a2f46277b08970adb016a72dcbd
Shatakshi Rajput <shatakshirajput02@gmail.com>
2026-06-19 10:11:56 +0530

    graph

--- Graph/CycleDetection/ModifiedDFS.java
+++ Graph/CycleDetection/ModifiedDFS.java
@@ -16,16 +16,16 @@ public class ModifiedDFS {
 
     public static void createGraph(ArrayList<Edge> graph[]){
         for(int i = 0;i < graph.length; i++){
-            graph[i] = new ArrayList<>();
+            graph[i] = new ArrayList<Edge>();
         }
 
         graph[0].add(new Edge(0, 1));
         graph[0].add(new Edge(0, 2));
         graph[1].add(new Edge(1, 0));
-        graph[1].add(new Edge(1, 2));
+        // graph[1].add(new Edge(1, 2));
         graph[1].add(new Edge(1, 3));
         graph[2].add(new Edge(2, 0));
-        graph[2].add(new Edge(2, 1));
+        // graph[2].add(new Edge(2, 1));
         graph[3].add(new Edge(3, 1));
         graph[3].add(new Edge(3, 4));
         graph[4].add(new Edge(4, 3));
@@ -35,7 +35,7 @@ public class ModifiedDFS {
         boolean[] visited = new boolean[graph.length];
         for(int i = 0;i < graph.length;i++){
             if(!visited[i]){
-                if(detectCycle(graph)){
+                if(detectCycleUtil(graph,visited,i,-1)){
                     return true;
                 }
             }
@@ -43,7 +43,27 @@ public class ModifiedDFS {
         return false;
     }
 
-    public static void main(String[] args){
+    public static boolean detectCycleUtil(ArrayList<Edge> graph[], boolean visited[], int curr,int parent ){
+        visited[curr] = true;
 
+        for(int i = 0;i < graph[curr].size();i++){
+            Edge e = graph[curr].get(i);
+            if(!visited[e.dest] ){
+                if(detectCycleUtil(graph,visited,e.dest,curr)){
+                    return true;
+                }
+            }
+            else if(visited[e.dest] && e.dest != parent){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args){
+        int V = 5;
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        createGraph(graph);
+        System.out.println(detectCycle(graph));
     }
 }
