f17b45e9939d67d8423c1bdc0c6a80881f407e87
Shatakshi Rajput <shatakshirajput02@gmail.com>
2026-06-27 21:57:42 +0530

    graph

--- Graph/ConnectingCities.java
+++ Graph/ConnectingCities.java
@@ -1,7 +1,52 @@
 package Graph;
+import java.util.*;
 
 public class ConnectingCities {
-    public static void main(String[] args) {
-               
+    static class Edge implements Comparable<Edge>{
+        int dest;
+        int cost;
+        
+        public Edge(int d, int c){
+            this.dest = d;
+            this.cost = c;
+        }    
+
+        @Override
+        public int compareTo(Edge e2){
+            return this.cost - e2.cost;
+        }
+    }
+
+    public static int connectCities(int cities[][]){
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        boolean vis[] = new boolean[cities.length];
+
+        pq.add(new Edge(0, 0));
+        int cost  = 0;
+
+        while(!pq.isEmpty()){
+            Edge curr = pq.remove();
+            if(!vis[curr.dest]){
+                vis[curr.dest] = true;
+                cost += curr.cost;
+                for(int i =0;i < cities[curr.dest].length;i++){
+                    if(cities[curr.dest][i] != 0){
+                        pq.add(new Edge(i, cities[curr.dest][i]));
+                    }
+                }
+            }
+        }
+
+        return cost;
+    }
+
+    public static void main(String[] args){
+        int cities[][] = {{0,1,2,3,4},
+                          {1,0,5,0,7},
+                          {2,5,0,6,0},
+                          {3,0,6,0,0},
+                          {4,7,0,0,0}};
+
+        System.out.println(connectCities(cities));
     }
 }
