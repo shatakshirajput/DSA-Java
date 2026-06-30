aa5d287729ff09e013614037a44f029fbec2f32f
Shatakshi Rajput <shatakshirajput02@gmail.com>
2026-06-30 08:58:45 +0530

    kruskalsalgo

--- Graph/KrushkalsAlgo.java
+++ Graph/KrushkalsAlgo.java
@@ -19,7 +19,7 @@ public class KrushkalsAlgo {
         }
     }
 
-    static void createGRaph(ArrayList<Edge> edges){
+    static void createGraph(ArrayList<Edge> edges){
         edges.add(new Edge(0, 1, 10));
         edges.add(new Edge(0, 2, 15));
         edges.add(new Edge(0, 3, 30));
@@ -29,5 +29,57 @@ public class KrushkalsAlgo {
 
     static int n = 4;
     static int par[] = new int[n];
+    static int rank[] = new int[n];
 
+    public static void init(){
+        for(int i = 0;i < n;i++){
+            par[i] = i;
+        }
+    }
+
+    public static int find(int x){
+        if(par[x] == x){
+            return x;
+        }
+        return par[x] = find(par[x]);
+    }
+
+    public static void union(int a, int b){
+        int parA = find(a);
+        int parB = find(b);
+        if(rank[parA] == rank[parB]){
+            par[parB] = parA;
+            rank[parA]++;
+        }else if(rank[parA] < rank[parB]){
+            par[parA] = parB;
+        }else{
+            par[parB] = parA;
+        }
+    }
+
+    public static void kruskalMST(ArrayList<Edge> edges,int V){
+        init();
+        Collections.sort(edges);
+        int mst = 0;
+        int count = 0;
+        for(int i = 0;count < V-1;i++){
+            Edge e = edges.get(i);
+            int parA= find(e.src);
+            int parB = find(e.dest);
+            if(parA != parB){
+                union(e.src,e.dest);
+                mst +=e.wt;
+                count++;
+            }
+        }
+        System.out.println(mst);
+    }
+
+    public static void main(String[] args){
+        int V = 4;
+        ArrayList<Edge> edges = new ArrayList<>();
+        createGraph(edges);
+
+        kruskalMST(edges, V);
+    }
 }
